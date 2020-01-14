3c393c384cf5caa94fb5945fdd29c0e3d17ee89b
yhChen <kchenyuhang@gmail.com>
2020-01-14 17:34:21 +0800

    Day2——IO流文件复制

--- src/com/soft/week1/FileCopy.java
+++ src/com/soft/week1/FileCopy.java
@@ -4,34 +4,40 @@ import java.io.*;
 import java.util.UUID;
 
 /**
+ * 使用缓冲流，实现文本文件和非文本文件的拷贝
+ *
  * @author yhChen
  * @date 2020.1.14
  */
 public class FileCopy {
     public static void main(String[] args) {
+        //非文本文件拷贝
         try {
             //指定源文件
-            File startFile = new File("D:/资源/pictures/P1.jpg");
+            File startFile = new File("D:/FileOutPut/RandomStudy.txt");
             String startFileName = startFile.getName();
             int position = startFileName.indexOf(".");
-            //获得文件扩展名
+            //取得文件扩展名
             String suffixName = startFileName.substring(position+1);
-            //随机生成新的文件名
+            //新的文件名
             String endName = UUID.randomUUID().toString();
             //指定文件复制到的路径
             File endFile = new File("D:/FileOutPut/"+endName+"."+suffixName);
             byte[] bytes = new byte[(int) startFile.length()];
-            //字节输入流
-            InputStream fileInputStream = new FileInputStream(startFile);
-            int write = fileInputStream.read(bytes);
-            fileInputStream.close();
-            //字节输入流
-            OutputStream b = new FileOutputStream(endFile);
-            b.write(bytes);
-            b.close();
-            System.out.println("复制成功");
+
+            InputStream input = new FileInputStream(startFile);
+            OutputStream out = new FileOutputStream(endFile);
+            BufferedInputStream bufferedInputStream = new BufferedInputStream(input);
+            BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(out);
+            int temp = 0;
+            while ((temp=bufferedInputStream.read())!=-1){
+                bufferedOutputStream.write(temp);
+            }
+            bufferedInputStream.close();
+            bufferedOutputStream.close();
+            System.out.println("文件复制成功");
         } catch (IOException e) {
-            System.out.println("复制失败");
+            System.out.println("文件复制失败");
         }
     }
 }
